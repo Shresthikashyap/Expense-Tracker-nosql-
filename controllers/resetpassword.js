773b773ec3479013d973a5ae42fad19b80be6bdb
Shresthikashyap <sk2929542@gmail.com>
2023-06-04 16:02:33 +0530

    deploy

--- controllers/resetpassword.js
+++ controllers/resetpassword.js
@@ -35,7 +35,7 @@ const forgotpassword = async (req, res) => {
                 textContent: 'We received a request to reset the password for your account. Please follow the link below to reset your password:',
                 htmlContent: `<p>Hello,</p>
                 <p>We received a request to reset the password for your account. Please follow the link below to reset your password:</p>
-                <p><a href="http://13.239.34.74:3000/password/resetpassword/${id}">Reset Password</a></p><p>If you did not request this password reset, please ignore this email and contact us immediately.</p><p>Thank you,
+                <p><a href="http://localhost:3000/password/resetpassword/${id}">Reset Password</a></p><p>If you did not request this password reset, please ignore this email and contact us immediately.</p><p>Thank you,
                 </p><p>Expensify</p>`
             }
  
