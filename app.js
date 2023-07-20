434ac49f25dbb5c2344d15b1464b3d8780c94487
Anjali <anjaliradcliffe7@gmail.com>
2023-07-21 02:52:46 +0530

    deploy

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use((req,res) => {
 
 mongoose.connect('mongodb+srv://sk2929542:1234shit@cluster0.7ypej7u.mongodb.net/expense-tracker?retryWrites=true&w=majority')
 .then(()=>{
-    app.listen(3000,'13.51.197.130',()=>{
+    app.listen('13.51.197.130',()=>{
         console.log('server is listening');
     })
 })
